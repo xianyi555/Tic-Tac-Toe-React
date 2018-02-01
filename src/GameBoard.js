d988418844867bb7e403169df2733b0a706c662e
Xianyi555 <xianyi555@gmail.com>
2018-01-31 21:46:20 -0800

    finished the handleBoard function

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -16,12 +16,31 @@ export default class GameBoard extends Component {
 
   handleBoard(e) {
     e.preventDefault();
-    console.log("yay!!")
-    this.setState({ 
-      turn: 'O'
-    })
+    console.log(e.target.getAttribute("id"))
+    let id = e.target.getAttribute("id")
+    let input = document.getElementById(id)
+    
 
+    if (input.innerHTML === "") {
+      if (this.state.turn === "X") {
+        input.innerHTML = this.state.turn
+        this.setState({ 
+          turn: 'O'
+        })  
+      }else{
+        input.innerHTML = this.state.turn
+        this.setState({
+          turn: 'X'
+        })
+      }
+    }
   }
+
+  checkIfWin(e) {
+    e.preventDefault();
+    
+  }
+  
 
 
   render() {
@@ -33,19 +52,19 @@ export default class GameBoard extends Component {
               <div className="col-md-12">
                 <div className="col-md-6 col-md-offset-3" id="board">
                   <div className="row">
-                    <div className="col-md-4 box id1" onClick={ (e) => this.handleBoard(e) } onChange={ this.handleBoard }>+</div>
-                    <div className="col-md-4 box id2" onClick={ (e) => this.handleBoard(e) } onChange={ this.handleBoard }>+</div>
-                    <div className="col-md-4 box id3" onClick={ (e) => this.handleBoard(e) } onChange={ this.handleBoard }>+</div>
+                    <div className="col-md-4 box" id="id1" onClick={ (e) => this.handleBoard(e) } onChange={ this.handleBoard }></div>
+                    <div className="col-md-4 box" id="id2" onClick={ (e) => this.handleBoard(e) } onChange={ this.handleBoard }></div>
+                    <div className="col-md-4 box" id="id3" onClick={ (e) => this.handleBoard(e) } onChange={ this.handleBoard }></div>
                   </div>
                   <div className="row">
-                    <div className="col-md-4 box id4" onClick={ (e) => this.handleBoard(e) } onChange={ this.handleBoard }>+</div>
-                    <div className="col-md-4 box id5" onClick={ (e) => this.handleBoard(e) } onChange={ this.handleBoard }>+</div>
-                    <div className="col-md-4 box id6" onClick={ (e) => this.handleBoard(e) } onChange={ this.handleBoard }>+</div>
+                    <div className="col-md-4 box" id="id4" onClick={ (e) => this.handleBoard(e) } onChange={ this.handleBoard }></div>
+                    <div className="col-md-4 box" id="id5" onClick={ (e) => this.handleBoard(e) } onChange={ this.handleBoard }></div>
+                    <div className="col-md-4 box" id="id6" onClick={ (e) => this.handleBoard(e) } onChange={ this.handleBoard }></div>
                   </div>
                   <div className="row">
-                    <div className="col-md-4 box id7" onClick={ (e) => this.handleBoard(e) } onChange={ this.handleBoard }>+</div>
-                    <div className="col-md-4 box id8" onClick={ (e) => this.handleBoard(e) } onChange={ this.handleBoard }>+</div>
-                    <div className="col-md-4 box id9" onClick={ (e) => this.handleBoard(e) } onChange={ this.handleBoard }>+</div>
+                    <div className="col-md-4 box" id="id7" onClick={ (e) => this.handleBoard(e) } onChange={ this.handleBoard }></div>
+                    <div className="col-md-4 box" id="id8" onClick={ (e) => this.handleBoard(e) } onChange={ this.handleBoard }></div>
+                    <div className="col-md-4 box" id="id9" onClick={ (e) => this.handleBoard(e) } onChange={ this.handleBoard }></div>
                   </div>
                 </div>
               </div>
